a04e4f73b7b2e50290b53ee0a815c9c67d1251e5
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-01-25 23:54:35 +0100

    Added last word length problem

---
+++ Array String/Length of Last Word/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Length_of_Last_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "   fly me   to   the moon  ";
+
+            Console.WriteLine(LengthOfLastWord(s));
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+
+            List<string> worldLength = new List<string>();
+
+            for(int i = s.Length-1; i >=0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    worldLength.Add(s[i].ToString());
+
+                    if(i != 0)
+                    {
+                        if (s[i - 1] == ' ')
+                        {
+                            return worldLength.Count;
+                        }
+                    }
+
+                }
+            }
+
+            return worldLength.Count;
+        }
+    }
+}
