bbf20dbfa188641aff35cac3394cb208835419e3
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-01-27 02:59:38 +0100

    Words in reverse solved, prefix needs to be fixed

---
+++ Array String/Longest Common Prefix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Longest_Common_Prefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] { "ab", "a" };
+            string[] strsa = new string[] { "flos", "flow","fla" };
+            Console.WriteLine(LongestCommonPrefix(strsa));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            int count = 0;
+            int i = 0;
+            bool flag = true;
+
+            if (strs[0].ToString() == "")
+            {
+                return "";
+            }
+
+            string counter = strs[0][count].ToString();
+            string prefix = ""; 
+
+
+            while (flag)
+            {
+                if (strs[i][count].ToString() == counter && strs.Length > 1)
+                {
+                    i++;
+                    count = 0;
+                    flag = true;
+
+                    if (i == strs.Length)
+                    {
+                        i = 0;
+                        count++;
+                        prefix += counter;
+                        counter = strs[i][count].ToString();
+                        
+                    }
+                }               
+                else 
+                {
+                    flag = false;
+                    
+                    return prefix;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Array String/Reverse Words in a String/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Reverse_Words_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Te amo Zazil";
+
+            Console.WriteLine(ReverseWords(s));
+        }
+
+        public static string ReverseWords(string s)
+        {
+            string word = "";
+            string backwardWord = "";
+            List<string> words = new List<string>();
+
+            for(int i = 0; i<s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    word += s[i];
+                }
+                if(word != "" && (s[i] == ' ' || i == s.Length-1 ) )
+                {
+                    words.Add(word);
+                    word = "";
+                }
+            }
+
+           
+
+            for(int i = words.Count-1; i >= 0; i--)
+            {
+                backwardWord += words[i] + ' ';
+                if(i == 0)
+                {
+                    backwardWord = backwardWord.Remove(backwardWord.Length-1);
+                }
+            }
+
+            return backwardWord;
+        }
+    }
+}
