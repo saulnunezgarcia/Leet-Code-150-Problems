c4f6d69e1cbb6e72a7e5a7d8f66161583c09c56f
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-01-28 01:14:49 +0100

    Integers to romans added

---
+++ Array String/Integer to Roman/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Integer_to_Roman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IntToRoman(1994));
+        }
+
+        public static string IntToRoman(int num)
+        {
+            string[] ones = {"","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            string[] tens = { "","X","XX", "XXX","XL","L","LX","LXX","LXXX","XC"};
+            string[] cents = { "","C", "CC", "CCC","CD","D","DC","DCC","DCCC","CM" };
+            string[] thousands = {"","M","MM","MMM"};
+
+            string number = thousands[num/1000] + cents[num%1000/100] + tens[num%100/10] + ones[num%10];
+            return number;
+        }
+    }
+}
