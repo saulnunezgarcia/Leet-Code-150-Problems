1bcdd7eabbf18b40dfbfa073791fa140c16fd476
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-01-28 02:39:22 +0100

    Slicing window algorithm applied

---
+++ Array String/Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string haystack = "leetcode";
+            string needle = "leeto";
+            Console.WriteLine(StrStr(haystack,needle));
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            int windowSize = needle.Length;
+
+            for (int i = 0; i <= haystack.Length-windowSize; i++)
+            {
+                if (haystack.Substring(i, windowSize) == needle)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
