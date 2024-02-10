d38a8f11749c0d0d5c24536247a23e69b1706b8c
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-02-10 03:18:13 +0100

    Starting two pointers

---
+++ Array String/Gas Station/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Gas_Station
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] gas = { 5,1,2,3,4};
+            int[] cost = { 4,4,1,5,1};
+            Console.WriteLine(CanCompleteCircuit(gas,cost));
+        }
+
+        public static int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            int gasIndex = 0;
+            int costIndex = 0;
+            bool around = false;
+            int tank = 0;
+            int i = 0;
+            int valueDifference = 0;
+
+            for(; i < gas.Length; i++)
+            {
+                
+                if (gas[i] > cost[i] && gas[i] - cost[i] > valueDifference)
+                {
+                    valueDifference = gas[i] - cost[i];
+                    gasIndex = i;
+                    costIndex = i;
+                }
+            }
+
+            tank = gas[gasIndex];
+
+            while (!around)
+            {
+                if(gasIndex == gas.Length-1)
+                {
+                    gasIndex = 0;
+                }
+                else
+                {
+                    gasIndex++;
+                }
+
+                tank = tank - cost[costIndex] + gas[gasIndex];
+
+                if (gasIndex == gas.Length - 1)
+                {
+                    gasIndex = 0;
+                }
+                else
+                {
+                    gasIndex++;
+                }
+
+                if (tank >= gas[gasIndex] )
+                {
+                    around = false;
+                    gasIndex--;
+                }
+                else
+                {
+                    return -1;
+                    break;
+                    
+                }
+
+                costIndex++;
+
+                if (costIndex == cost.Length)
+                {
+                    costIndex = 0;
+                }
+
+                if (gasIndex == i)
+                {
+                    around = true;
+                    return gasIndex;
+                    break;
+                    
+                }
+                
+                
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Two Pointers/Valid Palindrome/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Valid_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Damosel, a poem? A carol? Or a cameo pale? (So mad!)";
+
+            Console.WriteLine(IsPalindrome(s));
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            
+            s = s.ToLower();
+
+            Regex reg = new Regex("[]*'\",`_?&!#^@{()}[.;: -]");
+            s = reg.Replace(s, string.Empty);
+
+            if (s.Length == 0)
+            {
+                return true;
+            }
+
+
+            int start = 0;
+            int end = s.Length-1;
+
+            //race
+            //ecar
+            for (int i = 0; i < s.Length/2; i++)
+            {
+                if (s[i] == s[end])
+                {
+                  
+                    end--;
+                }
+                else
+                {
+                    return false;                   
+                }
+            }
+
+            return true;
+        }
+    }
+}
