ae36cf5956b56b3a77b3d417d962cf01c0f21e99
saulnunezgarcia <saulgarcia.2398@gmail.com>
2024-02-10 22:28:51 +0100

    Is subsequence added

---
+++ Two Pointers/Is Subsequence/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Is_Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string t = "abc";
+            string s = "b";
+            Console.WriteLine(IsSubsequence(s,t));
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0) 
+            { 
+                return true; 
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[count])
+                {
+                    count++;
+                    if (count == s.Length)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
